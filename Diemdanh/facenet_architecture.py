133908dc5637f260c634d1357da6eac1cb307d14
PhuongAIT <130298468+PhuongAIT@users.noreply.github.com>
2024-09-13 18:01:45 +0700

    Update facenet_architecture.py

--- Diemdanh/facenet_architecture.py
+++ Diemdanh/facenet_architecture.py
@@ -1,5 +1,13 @@
 import tensorflow as tf
-from keras.layers import Conv2D, Activation, Input, Add, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, Concatenate, Lambda, add, GlobalAveragePooling2D, Convolution2D, LocallyConnected2D, ZeroPadding2D, concatenate, AveragePooling2D
+#from keras.layers import Conv2D, Activation, Input, Add, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, Concatenate, Lambda, add, GlobalAveragePooling2D, Convolution2D, LocallyConnected2D, ZeroPadding2D, concatenate, AveragePooling2D
+#from tensorflow.keras.layers import LocallyConnected2D, Conv2D, Activation, Input, Add, MaxPooling2D
+from tensorflow.keras.layers import (
+    Conv2D, Activation, Input, Add, MaxPooling2D, Flatten, Dense, Dropout,
+    BatchNormalization, Concatenate, Lambda, add, GlobalAveragePooling2D,
+    Convolution2D, LocallyConnected2D, ZeroPadding2D, concatenate,
+    AveragePooling2D
+)
+
 from keras.models import Model, Sequential
 from keras import backend as K
 
